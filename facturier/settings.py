a4dc4a069178b0ce278ebd6a82db68d58ce86a84
tlacolley <thomaslacolley@gmail.com>
2018-10-02 11:51:13 +0200

    DB creation with Postgre

--- facturier/settings.py
+++ facturier/settings.py
@@ -80,8 +80,10 @@ WSGI_APPLICATION = 'facturier.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'facturier',
+        'USER': 'facturier',
+        'PASSWORD': 'plop48000',
     }
 }
 
