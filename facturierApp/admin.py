4647bb886140ff525cad7f6bf56652b9dfd3b528
tlacolley <thomaslacolley@gmail.com>
2018-10-02 10:53:47 +0200

    Creation models and admin view. Migration Tables Done

--- facturierApp/admin.py
+++ facturierApp/admin.py
@@ -2,5 +2,75 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
-
+from .models import Client, Produit, LigneCmd, Devis, Facture
 # Register your models here.
+
+
+class ClientAdmin(admin.ModelAdmin):
+	list_display = ('firstname', 'lastname', 'slug', 'email', 'address', 'tel')
+	fieldsets = (
+		('General',{
+			'fields':(('firstname', 'lastname', 'slug'),
+					('email', 'address', 'tel')
+					),
+		} ),
+			)
+	prepopulated_fields = {'slug' : ('firstname', 'lastname')}
+
+	search_fields = ('firstname', 'lastname')
+
+
+class ProduitAdmin(admin.ModelAdmin):
+	list_display = ('name', 'slug', 'description', 'price', 'stock')
+	fieldsets = (
+		('General',{
+			'fields':(('name', 'slug'),
+					('price', 'stock')
+					),
+		} ),
+		('Content',{
+			'fields':(('description',)
+					),
+		} ),
+			)
+	prepopulated_fields = {'slug' : ('name',)}
+
+	search_fields = ('name', 'price')
+
+
+class LigneCmdAdmin(admin.ModelAdmin):
+	list_display = ('produit', 'quantity')
+
+
+class DevisAdmin(admin.ModelAdmin):
+	list_display = ('client', 'dateCreation', 'dateValid')
+	fieldsets = (
+		('General',{
+			'fields':(('client',),
+					('dateCreation', 'dateValid')
+					),
+		} ),
+			)
+
+	search_fields = ('client', 'dateCreation', 'dateValid')
+
+
+class FactureAdmin(admin.ModelAdmin):
+	list_display = ('client', 'dateCreation', 'dateValid', 'payment')
+	fieldsets = (
+		('General',{
+			'fields':(('client',),
+					('dateCreation', 'dateValid', 'payment')
+					),
+		} ),
+			)
+
+	search_fields = ('client', 'dateCreation', 'dateValid', 'payment')
+
+
+
+admin.site.register(Client,ClientAdmin)
+admin.site.register(Produit,ProduitAdmin)
+admin.site.register(LigneCmd,LigneCmdAdmin)
+admin.site.register(Devis,DevisAdmin)
+admin.site.register(Facture,FactureAdmin)
--- facturierApp/models.py
+++ facturierApp/models.py
@@ -1,6 +1,47 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.utils import timezone
 from django.db import models
 
 # Create your models here.
+
+class Client(models.Model):
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+    email = models.EmailField()
+    address = models.CharField(max_length=200)
+    tel = models.IntegerField()
+
+
+class Produit(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    price = models.FloatField()
+    stock = models.IntegerField()
+
+
+class LigneCmd(models.Model):
+    produit = models.ForeignKey(Produit, null=True, blank=True)
+    quantity = models.IntegerField()
+
+
+class Devis(models.Model):
+    listProduit = models.ForeignKey(LigneCmd, null=True, blank=True)
+    client = models.ForeignKey(Client, null=True, blank=True)
+    dateCreation = models.DateTimeField(auto_now_add=True,
+                                verbose_name="Date de creation")
+    dateValid = models.DateTimeField(auto_now=True,
+                                verbose_name="Date de validation")
+
+
+class Facture(models.Model):
+    listProduit = models.ForeignKey(LigneCmd, null=True, blank=True)
+    client = models.ForeignKey(Client, null=True, blank=True)
+    dateCreation = models.DateTimeField(auto_now_add=True,
+                                verbose_name="Date de creation")
+    dateValid = models.DateTimeField(auto_now=True,
+                                verbose_name="Date de validation")
+    payment = models.CharField(max_length=100)
